41f1842472067f1dd03f7ecb0ca4aa7d696346e7
Your Name <you@example.com>
2017-11-13 01:15:13 +0200

    end

--- script.js
+++ script.js
@@ -1,55 +1,78 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function() {
+ document.addEventListener("DOMContentLoaded", function() {
 
-    var inputValue = document.getElementById('inputValue').value;
+     var editedItem = null;
+
+     var input = document.getElementById('inputValue');
+
+     table.onclick = function (e) {
+
+         var target = e.target;
+         if (target.classList.contains('remove')) {
+
+             target.parentNode.parentNode.style.display = 'none';
+
+         } else if (target.classList.contains('edit')) {
+             // debugger;
 
 
-    var buttonAddRow = document.getElementById("buttonAddRow");
+            input.value = target.parentNode.previousElementSibling.textContent;
+            editedItem = target.parentNode.previousElementSibling.firstElementChild;
+            console.log(target.parentNode.previousElementSibling)
+         }
+     }
+
+    var goback = document.getElementsByClassName('goback')[0];
+    goback.onclick = function () {
+        input.value = '';
+        editedItem = null;
+
+    };
+
+
+     var buttonAddRow = document.getElementById("buttonAddRow");
      buttonAddRow.addEventListener("click", function () {
 
+         if(!input.value) return;
+
+         if(editedItem) {
+             editedItem.textContent = input.value;
+            input.value = '';
+            editedItem = null;
+         } else {
          var table = document.getElementById("table"),
+
              newrow = table.insertRow(table.length),
              cell1 = newrow.insertCell(0),
              cell2 = newrow.insertCell(1),
              cell3 = newrow.insertCell(2),
 
-             inputValue = document.getElementById('inputValue').value;
+             inputValue = input.value;
+
 
          var tagAremove = document.createElement("span");
          tagAremove.innerHTML = "remove";
+         tagAremove.classList.add("remove");
 
          var tagAredit = document.createElement("span");
          tagAredit.innerHTML = "edit";
+         tagAredit.classList.add("edit");
 
 
-         cell1.innerHTML = inputValue;
+             var createdSpan = document.createElement("span");
+             createdSpan.textContent = inputValue;
+
+             cell1.appendChild(createdSpan);
          cell2.appendChild(tagAredit);
          cell3.appendChild(tagAremove);
 
          // reset value in input after adding value to table
-         inputValue = document.getElementById('inputValue').value = '';
+         input.value = '';
 
-         var rem = document.getElementsByClassName('remove');
-         table.onclick = function (e) {
-
-             var target = e.target;
-             if (target.classList.contains('remove')) {
-
-                 target.parentNode.parentNode.style.display = 'none';
-
-             } else if (target.classList.contains('edit')) {
-
-
-                 e.target.innerHTML = inputValue.value;
-
-                 console.log(e.target.innerHTML)
-
-
-             }
          }
 
 
      });
- })
 
+ })
